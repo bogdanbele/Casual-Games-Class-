9e8ead8129c68a05e516c4cbdfe0f4d4cb1023cf
kineks <bogdan.bele@yahoo.com>
2016-05-31 23:15:39 +0200

    Chestii noi

--- EcoGame/js/intro.js
+++ EcoGame/js/intro.js
@@ -6,7 +6,6 @@
         ukd: false,
         dkd: false
     };
-
     var gameIsRunning = true;
     var player;
     var block = {};
@@ -20,19 +19,15 @@
     var speed = 3;
     var speedCurrent;
     var jumpTemp = 0;
-    var jumpSpeed = 20;
+    var jumpSpeed = 18;
     var isJumping = false;
     var mustTouchGround = false;
     var enemyKills = 0;
     var score = 0;
     var scoreText;
     var lifeText;
-
-
     var ground;
-    var ground2;
     var container;
-
     var mouseCheck = false;
     var weaponCooldown = 20;
     var weaponDuration = 1;
@@ -46,20 +41,17 @@
     var queue;
     var preloadText;
     var playerSS;
-
     var lifeFull = 5;
     var timeUntilSummon = 0;
     var timeUntilSummonMultiplier = 1;
     var timeUntilSummonMultiplierIncrease = 60;
     var summonMultiplierIncrease = 0.01;
 
-
-    var random = function(min, max) {
+    var random = function (min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     };
 
 }
-
 
 function resetWeapon() {
     weaponColide.x = 0;
@@ -124,13 +116,11 @@ function fingerUp(e) {
 }
 
 function mouseDown() {
-
     mouseCheck = true;
 }
 
 function mouseUp() {
     mouseCheck = false;
-
 }
 
 function charJump() {
@@ -168,7 +158,7 @@ function movePlayer() {
         mustTouchGround = true;
     }
     if (keys.dkd) {
-        player.y += speed*2;
+        player.y += speed * 2;
     }
 }
 
@@ -184,48 +174,39 @@ function playerMove() {
         })(i);
     }
 }
+
 function movePowerUp() {
-    for ( var i=0; i<powerUpList.length; i++){
+    for (var i = 0; i < powerUpList.length; i++) {
         var power = powerUpList[i];
-
-        if ( (power.y < 540-power.height ) && (power.levitate==false)){
-
+        if ((power.y < 540 - power.height ) && (power.levitate == false)) {
             power.y += power.speed;
-            power.speed=power.speed*1.01;
+            power.speed = power.speed * 1.01;
         }
-        else if ((power.y + power.height  > 460) && ( power.levitate==false)){
-   power.y = 520 -power.height;
-            power.levitate=true;
-
+        else if ((power.y + power.height > 460) && ( power.levitate == false)) {
+            power.y = 520 - power.height;
+            power.levitate = true;
         }
-
-
-
-
 
         if ((power.levitate) && (power.levCheck)) {
-            power.y ++;
+            power.y++;
             power.levAmount++;
 
         }
-         if ((power.levCheck)&&(power.levAmount >= 20 )) {
+        if ((power.levCheck) && (power.levAmount >= 20 )) {
             power.levCheck = false;
         }
-         if (power.levCheck == false ){
+        if (power.levCheck == false) {
             power.y--;
             power.levAmount--;
         }
-         if ((power.levCheck == false ) && ( power.levAmount < -20 )){
+        if ((power.levCheck == false ) && ( power.levAmount < -20 )) {
             power.levCheck = true;
         }
-
-
     }
 }
+
 function moveEnemies() {
     for (var i = 0; i < enemyList.length; i++) {
-
-
         var block = enemyList[i];
         if (block.enemyMove == true) {
             if (block.alive) {
@@ -235,7 +216,6 @@ function moveEnemies() {
                     if (block.y < block.posHeight - block.maxHeightDiff) {
                         block.goingUp = false;
                     }
-
                 }
                 else {
                     block.y += block.speedY;
@@ -243,41 +223,21 @@ function moveEnemies() {
                         block.goingUp = true;
                     }
                 }
-                /* var tempY = block.y;
-                 if (tempY == block.y ) {
-                 block.y--;
-                 if ((block.y + 5 ) == tempY){
-                 block.y++;
-                 }
-                 }*/
-
-
-                if (block.x < 0 - block.width) {
+                    if (block.x < 0 - block.width) {
                     block.container.removeChild(block);
                     block.alive = false;
-                    if ( block.name == "trash") {
+                    if (block.name == "trash") {
                         lifeFull--;
 
-
                     }
-                    else if ( block.name == "special" ){
-                        score+=1000;
+                    else if (block.name == "special") {
+                        score += 1000;
                     }
                     else {
-                        score+=100;
+                        score += 100;
                     }
                     resetPosition(block);
                     block.container.removeChild(block.healthText);
-                   /* switch(block.name){
-                        case "trash":
-                            lifeFull--;
-                            enemyCount++;
-                            break;
-                        default:
-                            break;
-                        }*/
-
-
                 }
             }
         }
@@ -287,7 +247,6 @@ function moveEnemies() {
     }
 }
 
-
 function hitTest(rect1, rect2) {
     /*  console.log(" rect1 x = " + rect1.x + " rect 2 .x " + rect2.x + " rect2 width  " + rect2.width + " rect2 height " + rect2.height)*/
     if (rect1.x >= rect2.x + rect2.width
@@ -296,87 +255,56 @@ function hitTest(rect1, rect2) {
         || rect1.y + rect1.height <= rect2.y) {
         return false;
     }
-
     return true;
 }
 
-
-/*
-
- function collide() {
- if (hitTest(block, player)) {
- }
- if (hitTest(block, weaponColide)) {
- for (var i = 1; i < enemyList.length; i++) {
-
- if (block.id == i) {
- block.currHealth -= weaponDamage;
- console.log(block.currHealth);
- damageEnemy(block);}
- block.healthDamage = true;
- }
-
- }
- }
- */
-
 function collidePowerUp() {
-    for (var i = 0; i < powerUpList.length; i++)
-    {
+    for (var i = 0; i < powerUpList.length; i++) {
         var powerCollider = {
-            x : powerUpList[i].x,
-            y : powerUpList[i].y ,
-            width : powerUpList[i].width,
-            height : powerUpList[i].height
+            x: powerUpList[i].x,
+            y: powerUpList[i].y,
+            width: powerUpList[i].width,
+            height: powerUpList[i].height
         };
-
-        if (hitTest(powerCollider, player)){
-            speed += 0.01;
-          container.removeChild(powerUpList[i]);
+        if (hitTest(powerCollider, player)) {
+            speed += 0.17;
+            container.removeChild(powerUpList[i]);
+            resetPosition(powerUpList[i]);
         }
     }
 }
 
 function collideBlock() {
-
     for (var i = 0; i < enemyList.length; i++) {
-
         var currCollider = {
             x: enemyList[i].x + enemyList[i].container.x,
             y: enemyList[i].y,
             width: enemyList[i].width,
             height: enemyList[i].height
         };
-
         if (hitTest(currCollider, weaponColide)) {
-
-
             enemyList[i].currHealth -= weaponDamage;
             /*      console.log(enemyList[i].currHealth);*/
             damageEnemy(enemyList[i]);
             enemyList[i].healthDamage = true;
         }
-
-
     }
 }
-
 
 function damageEnemy(event) {
     if (event.currHealth < 1) {
         event.container.removeChild(event);
         event.alive = false;
-        if ( event.name == "trash") {
-         score+=10;
+        if (event.name == "trash") {
+            score += 10;
             enemyKills++;
-            if (enemyKills % 5 == 0 ){
+            if (enemyKills % 2 == 0) {
                 summonPowerUp(event.x, event.y);
             }
             console.log(enemyKills);
-
         }
-        else if ( event.name == "special" ){
-            score+=100;
+        else if (event.name == "special") {
+            score += 100;
         }
         else {
             lifeFull--;
@@ -384,12 +312,47 @@ function damageEnemy(event) {
         resetPosition(event);
         event.container.removeChild(event.healthText);
         delete enemyList[event];
-
-
     }
 }
+
 function summonPowerUp(posX, posY) {
     var power = new createjs.Shape();
+    power.type = random(1, 140);
+    power.name ="";
+console.log(power.type + " carnace ");
+    switch (power.type >= 0) {
+
+        case (power.type<20):
+            power.name = "speed";
+
+            break;
+        case (power.type<40):
+            power.name = "attackSpeed";
+
+            break;
+        case (power.type<60):
+            power.name = "damage";
+
+            break;
+        case (power.type<80):
+            power.name = "reinforce";
+
+            break;
+        case (power.type<100):
+            power.name = "life";
+
+            break;
+        case (power.type<120):
+            power.name = "jump";
+
+            break;
+        case (power.type<140):
+            power.name = "wave";
+
+            break;
+    }
+
+
     power.graphics.beginFill('#F00');
     power.graphics.drawRect(0, 0, 31, 31);
     power.width = 31;
@@ -404,29 +367,22 @@ function summonPowerUp(posX, posY) {
     powerUpList.push(power);
 }
 
-
 function summonEnemy() {
-
-
     var block = new createjs.Shape();
     block.type = random(1, 100);
     block.name = "";
-    if( block.type <= 20 ) {
+    if (block.type <= 20) {
         block.name = "fish";
     }
-    else if ( block.type >= 95 ) {
+    else if (block.type >= 95) {
         block.name = "special";
     }
-    else if ( 20 < block.type < 95 ) {
+    else if (20 < block.type < 95) {
         block.name = "trash";
     }
-
-
-
-    switch(block.name){
+    switch (block.name) {
         case "fish":
             console.log(block.name + " random number is : " + block.type);
-
             block.graphics.beginFill('#bFF');
             block.graphics.drawRect(0, 0, 60, 40);
             block.width = 60;
@@ -437,19 +393,16 @@ function summonEnemy() {
             break;
         case "trash":
             console.log(block.name + " random number is : " + block.type);
-
             block.graphics.beginFill('#000');
             block.graphics.drawRect(0, 0, 20, 40);
             block.width = 20;
             block.height = 40;
-            block.speedY = Math.floor(Math.random() * ((2*(timeUntilSummonMultiplier) - 1) + 1) + 1);
-            block.speedX = Math.floor(Math.random() * ((2*(timeUntilSummonMultiplier) - 1) + 1) + 1);
+            block.speedY = Math.floor(Math.random() * ((2 * (timeUntilSummonMultiplier) - 1) + 1) + 1);
+            block.speedX = Math.floor(Math.random() * ((2 * (timeUntilSummonMultiplier) - 1) + 1) + 1);
 
             break;
         case "special":
             console.log(block.name + " random number is : " + block.type);
-
-
             block.graphics.beginFill('#FFF');
             block.graphics.drawRect(0, 0, 70, 70);
             block.width = 70;
@@ -462,14 +415,10 @@ function summonEnemy() {
             break;
     }
     block.x = 800;
-
     block.maxHeightDiff = Math.floor(Math.random() * ((200 - 5) + 1) + 5);
-    block.y = Math.floor(Math.random() * (((540-block.height - block.maxHeightDiff) - (block.maxHeightDiff)) + 1) + block.maxHeightDiff);
+    block.y = Math.floor(Math.random() * (((540 - block.height - block.maxHeightDiff) - (block.maxHeightDiff)) + 1) + block.maxHeightDiff);
     block.goingUp = Math.random() <= 0.5;
     block.posHeight = block.y;
-
-
-
     block.health = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
     block.currHealth = block.health;
     block.alive = true;
@@ -478,10 +427,8 @@ function summonEnemy() {
     block.container = container;
     block.healthDamage = true;
     container.addChild(block.healthText);
-
     container.addChild(block);
     enemyList.push(block);
-
 }
 
 function resetPosition(event) {
@@ -521,7 +468,6 @@ function preload() {
     queue = new createjs.LoadQueue(true);
     queue.on("progress", progress);
     queue.on("complete", startGame);
-
     queue.loadManifest(
         [
             /*  "img/1.jpg", "img/star.png",*/
@@ -529,7 +475,6 @@ function preload() {
         ]
     );
 }
-
 
 function progress(e) {
     var percent = Math.round(e.progress * 100);
@@ -543,30 +488,20 @@ function startGame() {
     stage.removeChild(preloadText);
     playerSS = new createjs.SpriteSheet(queue.getResult("js/playerrun.json"));
     console.log(playerSS);
-
     player = new createjs.Sprite(playerSS, 'right');
     player.width = 20;
     player.height = 40;
     player.x = 50;
-    player.y = 540-player.height;
-
-
+    player.y = 540 - player.height;
     scoreText = new createjs.Text('Score : ', "30px Courier", "#FFF");
     scoreText.y = 50;
     lifeText = new createjs.Text('Text : ', "30px Courier", "#FFF");
     //healthText = new createjs.Text('Health :', "30px Calibri", "#0F0");
-
-
     container = new createjs.Container();
     ground = new createjs.Bitmap("img/ground.png");
     ground.x = 0;
     ground.y = 540;
-    ground2 = new createjs.Bitmap("img/ground.png");
-    ground2.x = 800;
-    ground2.y = 440;
-
-
-    container.addChild(ground2, ground);
+    container.addChild(ground);
     stage.addChild(container);
     stage.addChild(scoreText);
     stage.addChild(lifeText);
@@ -576,101 +511,77 @@ function startGame() {
     window.addEventListener('keyup', fingerUp);
     window.addEventListener("mousedown", mouseDown);
     window.addEventListener("mouseup", mouseUp);
-
     enemyList = [];
 }
 
 function tickHappened(e) {
     if (gameIsRunning) {
         console.log(timeUntilSummonMultiplier);
-    /*console.log(container.x);*/
-/*    console.log(timeUntilSummon + ", " + timeUntilSummonMultiplier);*/
-    timeUntilSummon--;
-    if (timeUntilSummon <= 0) {
-        summonEnemy();
-        summonEnemy();
-        summonEnemy();
-
-
-        timeUntilSummon = Math.floor(Math.random() * ((340 - 70) + 1) + 70);
-        timeUntilSummon /= timeUntilSummonMultiplier;
-
-    }
-    timeUntilSummonMultiplierIncrease--;
-    if (timeUntilSummonMultiplierIncrease <= 0 && timeUntilSummonMultiplier <= 5) {
-        timeUntilSummonMultiplier += summonMultiplierIncrease * timeUntilSummonMultiplier;
-        timeUntilSummonMultiplierIncrease = 200;
-    }
-
-    movePlayer();
-    if (isJumping || mustTouchGround) {
-        charJump();
-    }
-    if (player.y >= 540-player.height) {
-        mustTouchGround = false;
-        player.y = 540-player.height;
-    }
-    if (player.x < 0) {
-        player.x = 0;
-    }
-    if (ground.x < -800) {
-        ground.x = 0;
-    }
-    if (ground2.x < 0) {
-        ground2.x = 800;
-    }
-    /*    collide();*/
-
-
-    scoreText.text = "Score : " + score;
-    lifeText.text = "Life left " + lifeFull;
-
-    if (mouseCheck) {
-        if (weaponDuration == 0) {
-            weaponAttack();
+        timeUntilSummon--;
+        if (timeUntilSummon <= 0) {
+            summonEnemy();
+            summonEnemy();
+            summonEnemy();
+            timeUntilSummon = Math.floor(Math.random() * ((340 - 70) + 1) + 70);
+            timeUntilSummon /= timeUntilSummonMultiplier;
         }
-    }
-
-    weaponDuration--;
-    if (weaponDuration < 0) {
-        weaponDuration = 0;
-    }
-
-    if (attackCheck) {
-        attackTimer++;
-    }
-
-    if (attackTimer == 2) {
-        attackTimer = 0;
-        stage.removeChild(weaponColide);
-        resetWeapon();
-        attackCheck = false;
-    }
-
-    for (var i = 0; i < enemyList.length; i++) {
-        var block = enemyList[i];
-
-        if ((( block.health - block.currHealth) >= block.health) && (block.healthDamage == true)) {
-
-            block.healthDamage = false;
+        timeUntilSummonMultiplierIncrease--;
+        if (timeUntilSummonMultiplierIncrease <= 0 && timeUntilSummonMultiplier <= 5) {
+            timeUntilSummonMultiplier += summonMultiplierIncrease * timeUntilSummonMultiplier;
+            timeUntilSummonMultiplierIncrease = 200;
         }
-        else if ((( block.health - block.currHealth) > block.health / 2) && (block.healthDamage == true)) {
-
-            block.healthDamage = false;
-            block.healthText.color = "#f00";
-
+        movePlayer();
+        if (isJumping || mustTouchGround) {
+            charJump();
         }
+        if (player.y >= 540 - player.height) {
+            mustTouchGround = false;
+            player.y = 540 - player.height;
+        }
+        if (player.x < 0) {
+            player.x = 0;
+        }
+        if (ground.x < -800) {
+            ground.x = 0;
+        }
+        scoreText.text = "Score : " + score;
+        lifeText.text = "Life left " + lifeFull;
+        if (mouseCheck) {
+            if (weaponDuration == 0) {
+                weaponAttack();
+            }
+        }
+        weaponDuration--;
 
-
-    }
-        if ( lifeFull <= 0 ){
+        if (weaponDuration < 0) {
+            weaponDuration = 0;
+        }
+        if (attackCheck) {
+            attackTimer++;
+        }
+        if (attackTimer == 2) {
+            attackTimer = 0;
+            stage.removeChild(weaponColide);
+            resetWeapon();
+            attackCheck = false;
+        }
+        for (var i = 0; i < enemyList.length; i++) {
+            var block = enemyList[i];
+            if ((( block.health - block.currHealth) >= block.health) && (block.healthDamage == true)) {
+                block.healthDamage = false;
+            }
+            else if ((( block.health - block.currHealth) > block.health / 2) && (block.healthDamage == true)) {
+                block.healthDamage = false;
+                block.healthText.color = "#f00";
+            }
+        }
+        if (lifeFull <= 0) {
             gameIsRunning = false;
         }
-
-    moveEnemies();
-    movePowerUp();
+        moveEnemies();
+        movePowerUp();
         collidePowerUp();
-    stage.update(e);
+        stage.update(e);
 
     }
 }
